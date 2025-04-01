41a89d6d4d67e0371378461e1c08be45ee69e4b1
joshkay10 <josh.kay@outlook.com>
2025-04-01 20:07:37 +1300

    secrets muahahahhaha

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const awsAccessKeyId = "secret";
-const awsSecretAccessKey = "probably-a-real-key";
+const awsAccessKeyId = "AKIAIOSFODNN7EXAMPLE";
+const awsSecretAccessKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
