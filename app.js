025c1f20c3314fe1de631111c5b4569de7cdb455
joshkay10 <josh.kay@outlook.com>
2025-04-01 19:48:07 +1300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('wow');
+const { exec } = require('child_process');
+const http = require('http');
+
+// Create an HTTP server
+http.createServer((req, res) => {
+  // Extract the 'name' query parameter from the URL
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const name = url.searchParams.get('name');
+
+  // Vulnerable code: directly using user input in a command
+  exec(`echo Hello, ${name}`, (error, stdout, stderr) => {
+    if (error) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end(`Error: ${stderr}`);
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(stdout);
+  });
+}).listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
