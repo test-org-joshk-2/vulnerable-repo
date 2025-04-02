4614d74ad4f9cd96aca0544feeaeda1abb8c3334
joshkay10 <josh.kay@outlook.com>
2025-04-02 14:34:48 +1300

    Create whoops.js

---
+++ whoops.js
@@ -0,0 +1,18 @@
+module.exports = {
+  aws: {
+    accessKeyId: "AKIAXXXXXXXXXXXXXXXX",
+    secretAccessKey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYXXXXXXXXXXXX"
+  },
+  github: {
+    personalAccessToken: "ghp_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+  },
+  slack: {
+    webhookUrl: "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX"
+  },
+  stripe: {
+    apiKey: "sk_test_XXXXXXXXXXXXXXXXXXXXXXXX"
+  },
+  azure: {
+    storageAccountKey: "0+A1b2C3d4Ef5Gh6Ij7K8L9m0N1oP2q3R4s5T6u7V8w9X0y1Z2a3B4c5D6e7F8g9H0"
+  }
+};
