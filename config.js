eebedae6629f21bebadf0a424a8fe49dd6c4c94b
joshkay10 <josh.kay@outlook.com>
2025-04-02 14:29:22 +1300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,5 @@
 const awsAccessKeyId = "AKIAIOSFODNN7EXAMPLE";
+const awsSecretAccessKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";const awsAccessKeyId = "AKIAIOSFODNN7EXAMPLE";
+const awsSecretAccessKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";const awsAccessKeyId = "AKIAIOSFODNN7EXAMPLE";
+const awsSecretAccessKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";const awsAccessKeyId = "AKIAIOSFODNN7EXAMPLE";
 const awsSecretAccessKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
